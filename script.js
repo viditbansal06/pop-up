a46025829c6cedf7b73e5e9ebe645a2aa9d66eee
Vidit <viditbansal0106@gmail.com>
2022-07-16 00:09:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    var check = false
+    $('.clicks').on('click', function () {
+      if (check == false) {
+        $('#box').addClass('show')
+        check = true
+      } else {
+        $('#box').removeClass('show')
+        check = false
+      }
+    })
+  
+    $('.submission').on('click', function () {
+      $('#box').removeClass('show')
+      check = false
+    })
+  
+    $('.close_popup').on('click', function () {
+      $('#box').removeClass('show')
+      check = false
+    })
+  })
